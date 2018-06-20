de0082dfde92f71274c69163eecc8f1c1ae25298
yxmline <yxmline@live.cn>
2018-06-20 12:55:25 +0800

    Update version.sh

--- version.sh
+++ version.sh
@@ -13,13 +13,13 @@ export SS_LIBEV_LINK="https://github.com/shadowsocks/shadowsocks-libev/releases/
 export SS_LIBEV_YUM_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/ss_libev.init"
 export SS_LIBEV_APT_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/ss_libev_apt.init"
 # SSR
-#export SSR_VER=3.3.2
+#export SSR_VER=3.4.0
 export SSR_VER=$(wget --no-check-certificate -qO- https://raw.githubusercontent.com/shadowsocksrr/shadowsocksr/manyuser/shadowsocks/version.py | grep return | cut -d\' -f2 | awk '{print $1}')
 export SSR_LINK="https://github.com/onekeyshell/shadowsocksr/archive/manyuser.zip"
 export SSR_YUM_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/ssr.init"
 export SSR_APT_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/ssr_apt.init"
 # SSRR
-#export SSRR_VER=3.2.1
+#export SSRR_VER=3.2.2
 export SSRR_VER=$(wget --no-check-certificate -qO- https://raw.githubusercontent.com/shadowsocksrr/shadowsocksr/akkariiin/dev/shadowsocks/version.py | grep return | cut -d\' -f2 | awk '{print $2}')
 export SSRR_LINK="https://github.com/shadowsocksrr/shadowsocksr/archive/akkariiin/dev.zip"
 export SSRR_YUM_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/ssrr.init"
